c0d383e734e0027e7e5a2ab4d148a20b8430a78c
Tahv <werydude@gmail.com>
2024-03-15 16:48:14 -0700

    Initial API for matchups

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,31 @@
 use godot::prelude::*;
+use godot::engine::{Sprite2D, ISprite2D};
+use crate::rps::matchup::{self, *};
+
+pub mod rps;
 
 struct MyExtension;
 
 #[gdextension]
 unsafe impl ExtensionLibrary for MyExtension {}
+
+#[derive(GodotClass)]
+#[class(base=Sprite2D)]
+struct Throw {
+    sign: matchup::Sign,
+    base: Base<Sprite2D>
+}
+
+
+#[godot_api]
+impl ISprite2D for Throw {
+    fn init(base: Base<Sprite2D>) -> Self {
+        let s = format!("{:?}", fight(&Sign::Rock, &Sign::Scissors));
+        godot_print!("Rock attacking Scissors is a: {s}"); // Prints to the Godot console
+        
+        Self {
+            sign: Sign::Rock,
+            base,
+        }
+    }
+}
---
+++ rust/src/rps/matchup.rs
@@ -0,0 +1,49 @@
+use std::cmp::Ordering::{self, *};
+use godot::prelude::*;
+
+#[derive(PartialEq)]
+pub enum Sign {
+    Rock,
+    Paper,
+    Scissors
+}
+
+
+impl PartialOrd for Sign {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        if self == other{
+            return Some(Equal)
+        } else {
+            match (self, other) {
+                (Self::Rock, Self::Paper) => Some(Less),
+                (Self::Paper, Self::Scissors) => Some(Less),
+                (Self::Scissors, Self::Rock) => Some(Less),
+                (Self::Rock, Self::Scissors) => Some(Greater),
+                (Self::Paper, Self::Rock) => Some(Greater),
+                (Self::Scissors, Self::Paper) => Some(Greater),
+                _ => None
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Outcome {
+    Win,
+    Loss,
+    Tie
+}
+
+impl From<Ordering> for Outcome {
+    fn from(value: Ordering) -> Self{
+        match value {
+            Equal => Self::Tie,
+            Less => Self::Loss,
+            Greater => Self::Win,
+        }
+    }
+}
+
+pub fn fight(a: &Sign, b: &Sign) -> Outcome {
+    Sign::partial_cmp(a, b).unwrap().into()
+}
