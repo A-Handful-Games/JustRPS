fbd9a3eba28f578f485aa29b3195cc5f2765aba0
Tahv <werydude@gmail.com>
2024-03-18 22:38:41 -0700

    Initial networking test
    NOTE: Will hang godot; this is correct, but not intended. TBF.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,6 +3,7 @@ use godot::engine::{Node3D, INode3D};
 use crate::rps::matchup::{self, *};
 
 pub mod rps;
+pub mod networking;
 
 struct MyExtension;
 
@@ -22,7 +23,8 @@ impl INode3D for Throw {
     fn init(base: Base<Node3D>) -> Self {
         let s = format!("{:?}", fight(&Sign::Rock, &Sign::Scissors));
         godot_print!("Rock attacking Scissors is a: {s}"); // Prints to the Godot console
-        
+        godot_print!("Serving...");
+        let _ = networking::serve();
         Self {
             sign: Sign::Rock,
             base,
